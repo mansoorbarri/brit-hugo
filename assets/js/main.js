bc9681f7531ddb7f8b39c79b912a3cc68f2e19cb
Mansoor Barri <mansoor.eb.ak@gmail.com>
2024-02-15 01:46:14 +0000

    update: hugo content fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,4 @@ document.addEventListener('DOMContentLoaded', function () {
       cursor.style.top = e.pageY + 'px';
     });
 });
+
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,8 +2,20 @@
 const listItems = document.querySelectorAll('.menu li');
 
 // Add event listeners to each list item
-listItems.forEach((item) => {
+listItems.forEach((item, index) => {
     const spanElement = item.querySelector('span'); // Select the span element within each list item
+
+    if (index === 0) {
+        item.classList.add('active');
+        if (spanElement) {
+            spanElement.classList.add('active-border');
+        }
+    } else {
+        item.classList.add('unactive'); // Add 'unactive' class to other items
+        if (spanElement) {
+            spanElement.classList.add('unactive-border');
+        }
+    }
 
     item.addEventListener('mouseover', () => {
         // Add 'unactive' class to all items except the hovered one
