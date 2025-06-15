c63aed2f18a90d92178d324cff4d04ed931e8d1b
mansoorbarri <mansoor.eb.ak@gmail.com>
2025-06-15 03:12:40 +0100

    update README.md

---
+++ public/js/main.b20265ebfb90be52832a5ac316713b7541cf39e4c28c239b21acf9fa099ac8206480b64600ac8d34e63a0ce5400b120d7e8d022fd6d98ea55076f510b3511244.js
@@ -0,0 +1,10 @@
+// cursor 
+document.addEventListener('DOMContentLoaded', function () {
+    const cursor = document.querySelector('.cursor');
+  
+    document.addEventListener('mousemove', function (e) {
+      cursor.style.left = e.pageX + 'px';
+      cursor.style.top = e.pageY + 'px';
+    });
+});
+
